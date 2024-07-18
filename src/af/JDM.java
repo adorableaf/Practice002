c5ba17bb7f6c217938d6dd68fac7ef7c51b61dc2
adorableaf <hawlok2014@gmail.com>
2024-07-18 22:34:35 +0300

    add subclass

--- src/af/JDM.java
+++ src/af/JDM.java
@@ -1,32 +1,27 @@
 package af;
 
-public class JDM {
+public class JDM extends Transport {
 
-    private String name;
-    private float speed;
-    private float angle;
-    private int age;
-    private float weight;
-    private String desc;
+    private boolean isCool;
 
-    public JDM(String name, float speed, float angle, int age, float weight) {
-        setValues(name, speed, angle, age, weight, desc);
+    public JDM(String name, float speed, float angle) {
+        super(name, speed, angle);
     }
 
-    public JDM(String name, float speed, float angle, int age, float weight, String desc) {
-        setValues(name, speed, angle, age, weight, desc);
+    public JDM(String name, float speed, float angle, boolean isCool) {
+        super(name, speed, angle);
+        this.isCool = isCool;
     }
 
-    public void setValues(String name, float speed, float angle, int age, float weight, String desc){
-        this.name = name;
-        this.speed = speed;
-        this.angle = angle;
-        this.age = age;
-        this.weight = weight;
-        this.desc = desc;
+    public void setCool(boolean cool) {
+        isCool = cool;
     }
-    public String getValues() {
-        return "\n Название автомобиля: " + name + "\n Скорость автомобиля: " + speed + "\n Угол дрифта: " + angle + "\n Год выпуска: " + age + "\n Вес автомобиля: " + weight + "\n";
 
+    public void getCool() {
+        if (isCool) {
+            System.out.println("\tCool car");
+        } else {
+            System.out.println("\tCringe car");
+        }
     }
 }
